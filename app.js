0967280cc9b857d42aa221dcdbb1cdd923de7024
Sean Tindell <ravenn_706@hotmail.com>
2017-04-24 17:51:55 +0000

    Mlab connected

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var commentRoutes = require("./routes/comments"),
     indexRoutes = require("./routes/index");
     
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://ravenn706:Alpha706@ds051553.mlab.com:51553/yelpcamp");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
